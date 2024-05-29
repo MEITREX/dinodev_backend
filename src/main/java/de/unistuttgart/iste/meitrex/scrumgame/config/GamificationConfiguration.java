1534809cdb87b2d93204aa9c0ebfd88effab4395
PaulBredl <paulbredl@gmx.de>
2024-05-29 12:19:28 +0200

    add gamification engine

---
+++ src/main/java/de/unistuttgart/iste/meitrex/scrumgame/config/GamificationConfiguration.java
@@ -0,0 +1,63 @@
+package de.unistuttgart.iste.meitrex.scrumgame.config;
+
+import de.unistuttgart.iste.meitrex.generated.dto.CreateEventInput;
+import de.unistuttgart.iste.meitrex.generated.dto.Event;
+import de.unistuttgart.iste.meitrex.rulesengine.EventTypeRegistry;
+import de.unistuttgart.iste.meitrex.rulesengine.GamificationEngine;
+import de.unistuttgart.iste.meitrex.rulesengine.RuleRegistry;
+import de.unistuttgart.iste.meitrex.rulesengine.util.EventPublisher;
+import de.unistuttgart.iste.meitrex.scrumgame.ims.ImsEventTypes;
+import de.unistuttgart.iste.meitrex.scrumgame.service.gamification.EventPersistenceService;
+import de.unistuttgart.iste.meitrex.scrumgame.service.gamification.rules.SimpleTestRule;
+import de.unistuttgart.iste.meitrex.scrumgame.service.vcs.VcsEventTypes;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Slf4j
+@Configuration
+public class GamificationConfiguration {
+
+    @Bean
+    @Scope("singleton")
+    public EventPublisher<Event, CreateEventInput> eventPublisher(EventPersistenceService eventService) {
+        return new EventPublisher<>(eventService);
+    }
+
+    @Bean
+    public EventTypeRegistry eventTypeRegistry() {
+        return new EventTypeRegistry();
+    }
+
+    @Bean
+    RuleRegistry ruleRegistry(SimpleTestRule simpleTestRule) {
+        RuleRegistry ruleRegistry = new RuleRegistry();
+
+        ruleRegistry.register(simpleTestRule);
+
+        return ruleRegistry;
+    }
+
+    @Bean
+    public GamificationEngine gamificationEngine(
+            EventPublisher<Event, CreateEventInput> eventPublisher,
+            EventTypeRegistry eventTypeRegistry,
+            RuleRegistry ruleRegistry
+    ) {
+        GamificationEngine engine = new GamificationEngine(eventPublisher, ruleRegistry, eventTypeRegistry);
+
+        registerEventTypes(engine.getEventTypeRegistry());
+
+        return engine;
+    }
+
+    private void registerEventTypes(EventTypeRegistry eventTypeRegistry) {
+
+        ImsEventTypes.allEventTypes().forEach(eventTypeRegistry::register);
+        VcsEventTypes.allEventTypes().forEach(eventTypeRegistry::register);
+
+        // TODO add other event types
+    }
+
+}
