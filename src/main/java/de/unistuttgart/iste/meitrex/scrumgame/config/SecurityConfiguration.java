002d29b90141a6d66f0d5628aa81a8ef1bc45555
PaulBredl <paulbredl@gmx.de>
2024-05-29 12:24:31 +0200

    update security config

--- src/main/java/de/unistuttgart/iste/meitrex/scrumgame/config/SecurityConfiguration.java
+++ src/main/java/de/unistuttgart/iste/meitrex/scrumgame/config/SecurityConfiguration.java
@@ -1,6 +1,6 @@
 package de.unistuttgart.iste.meitrex.scrumgame.config;
 
-import de.unistuttgart.iste.meitrex.scrumgame.service.auth.AuthConnector;
+import de.unistuttgart.iste.meitrex.scrumgame.service.auth.AudienceValidator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +9,21 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.util.*;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -25,14 +33,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
-
-    @Value("${meitrex.frontend.url}")
-    private String frontendUrl;
-
     @Bean
+    @Profile("dev")
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList(frontendUrl));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "accept",
                 "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
@@ -50,15 +55,21 @@ public class SecurityConfiguration {
     DefaultSecurityFilterChain springDevWebFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/graphql**")
+                        .ignoringRequestMatchers("/webhook**")
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 )
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                // disable CORS
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers(HttpMethod.OPTIONS, "/graphql**").permitAll()
+                        // allow access to the GraphQL endpoint (required for schema introspection)
+                        .requestMatchers("/graphql**").permitAll()
                         // for now: allow access to the WebSocket endpoint
                         .requestMatchers("/graphql-ws**").permitAll()
                         // allow access to the GraphiQL interface
                         .requestMatchers("/graphiql**").permitAll()
+                        // allow access to webhooks
+                        .requestMatchers(HttpMethod.POST, "/webhook**").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(withDefaults())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
@@ -75,10 +86,14 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 )
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .cors(cors -> cors.disable()) // todo enable
                 .authorizeHttpRequests(authz -> authz
                         // allow OPTIONS requests to the GraphQL endpoint
                         .requestMatchers(HttpMethod.OPTIONS, "/graphql**").permitAll()
+                        // for now: allow access to the WebSocket endpoint
+                        .requestMatchers("/graphql-ws**").permitAll()
+                        // allow access to the GraphiQL interface
+                        .requestMatchers("/graphiql**").permitAll()
                         .anyRequest().authenticated()) // All requests require authentication
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
                 .build();
@@ -86,8 +101,26 @@ public class SecurityConfiguration {
 
     @Bean
     @Profile({"prod", "dev"})
-    public JwtDecoder jwtDecoder(AuthConnector authConnector) {
-        return authConnector.getJwtDecoder();
+    public JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = initNimbusDecoder();
+        OAuth2TokenValidator<Jwt> withAudience = createAudienceValidator();
+
+        jwtDecoder.setJwtValidator(withAudience);
+
+        return jwtDecoder;
+    }
+
+    @Value("${gropius.auth.secret}")
+    private String secret;
+
+    private OAuth2TokenValidator<Jwt> createAudienceValidator() {
+        return new DelegatingOAuth2TokenValidator<>(JwtValidators.createDefault(),
+                new AudienceValidator("backend"));
+    }
+
+    private NimbusJwtDecoder initNimbusDecoder() {
+        SecretKey secretKey = new SecretKeySpec(Base64.getDecoder().decode(secret), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 
 }
